4efe5d8f32ca81f5efe8370b365a91d0e0ab1fee
binxproject <iconiclampard99@gmail.com>
2025-04-12 17:11:52 +0700

    fix styling design, add feature scroll to top, add feature alert for empty search

--- public/script.js
+++ public/script.js
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchKeyword = document.getElementById('searchKeyword');
     const inputKeyword = document.getElementById('inputKeyword');
     const title = document.getElementById('title');
+    const btnScrollToTop = document.getElementById('btn-scroll-to-top');
+    const alertSearch = document.getElementById('alert-search');
 
     async function getMovies(url) {
         const res = await fetch(url)
@@ -29,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if(page > 1) {
             page -= 1;
             updatePage();
+            scrollToTop();
         }
     }
 
@@ -36,6 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if(page >= 1) {
             page += 1;
             updatePage();
+            scrollToTop();
         }
     }
 
@@ -57,12 +61,12 @@ document.addEventListener('DOMContentLoaded', () => {
         movies.forEach(movie => {
             const {title, poster_path, overview} = movie;
             const movieCard = document.createElement('div');
-            movieCard.className = 'bg-slate-800 w-full p-4 md:rounded-sm xl:rounded-lg';
+            movieCard.className = 'bg-neutral-800 w-full p-4 md:rounded-sm xl:rounded-lg';
             movieCard.innerHTML = ` <img src="${API_IMAGE_URL}${poster_path}" alt="Gambar ${title}" class="max-w-52 md:max-w-60 h-auto mx-auto">
-                                    <h3 class="text-white mt-2 text-center mb-1 md:text-xl">${title}</h3>
-                                    <p class="text-slate-400 text-justify md:text-xl">${overview.substring(0,200)}...</p>
+                                    <h3 class="text-white mt-2 text-center mb-1">${title}</h3>
+                                    <p class="text-gray-400 text-justify">${overview.substring(0,200)}...</p>
                                     <div class="wrap-btn flex justify-end">
-                                        <button class="text-white bg-slate-950 py-1.5 px-3 cursor-pointer hover:bg-slate-900 rounded-sm mt-2 md:py-2 md:px-4 md:text-xl">Show Detail</button>
+                                        <button class="text-white bg-indigo-600 py-1.5 px-3 cursor-pointer hover:bg-indigo-700 active:bg-indigo-800 rounded-sm mt-2 md:py-2 md:px-4">Show Detail</button>
                                     </div>`;
             moviesContainer.appendChild(movieCard);
         })
@@ -78,9 +82,38 @@ document.addEventListener('DOMContentLoaded', () => {
             getMovies(API_SEARCH_URL + inputKeyword.value);
             inputKeyword.value = '';
         } else {
-            alert('kolom pencarian tidak boleh kosong!')
+            alertSearch.classList.remove('hidden');
+            alertSearch.classList.add('opacity-100');
+            setTimeout(() => {
+                alertSearch.classList.remove('opacity-100');
+                alertSearch.classList.add('opacity-0');
+                setTimeout(() => {
+                    alertSearch.classList.add('hidden');
+                }, 500)
+            }, 3000)
+            
         }
+    });
+
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 300) {
+            btnScrollToTop.classList.remove('hidden');
+        } else {
+            btnScrollToTop.classList.add('hidden');
+        }
+    });
+
+    btnScrollToTop.addEventListener('click', () => {
+        scrollToTop();
     })
+
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0
+        });
+    };
+    
 
 
 
