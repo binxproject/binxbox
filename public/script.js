b8c64de1b0ac71a34a0df106795f18babc1dfc63
binxproject <iconiclampard99@gmail.com>
2025-04-13 18:04:02 +0700

    update code

--- public/script.js
+++ public/script.js
@@ -15,6 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const title = document.getElementById('title');
     const btnScrollToTop = document.getElementById('btn-scroll-to-top');
     const alertSearch = document.getElementById('alert-search');
+    const modalBoxContainer = document.getElementById('modal-box-container');
+    
+    
 
     async function getMovies(url) {
         const res = await fetch(url)
@@ -52,23 +55,29 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     function showMovies(movies) {
-        if (!moviesContainer) {
-            console.error('⛔ ID #movies-container tidak ditemukan. Pastikan id-nya benar dan elemen ada di HTML!');
-        } else {
-            console.log('✅ #movies-container ditemukan.');
-        }
         moviesContainer.innerHTML = '';
+
+        const notFound = document.getElementById('not-found');
+        console.log(movies.length)
+        if(movies.length === 0) {
+            notFound.classList.remove('hidden');
+            notFound.classList.add('flex');
+        } else {
+            notFound.classList.add('hidden');
+            notFound.classList.remove('flex');
+        }
+
         movies.forEach(movie => {
             const {title, poster_path, overview} = movie;
             const movieCard = document.createElement('div');
-            movieCard.className = 'bg-neutral-800 w-full p-4 md:rounded-sm xl:rounded-lg';
+            movieCard.className = 'bg-neutral-800 w-full p-4 md:rounded-sm xl:rounded-lg relative';
             movieCard.innerHTML = ` <img src="${API_IMAGE_URL}${poster_path}" alt="Gambar ${title}" class="max-w-52 md:max-w-60 h-auto mx-auto">
                                     <h3 class="text-white mt-2 text-center mb-1">${title}</h3>
-                                    <p class="text-gray-400 text-justify">${overview.substring(0,200)}...</p>
-                                    <div class="wrap-btn flex justify-end">
-                                        <button class="text-white bg-indigo-600 py-1.5 px-3 cursor-pointer hover:bg-indigo-700 active:bg-indigo-800 rounded-sm mt-2 md:py-2 md:px-4">Show Detail</button>
-                                    </div>`;
+                                    <p class="text-gray-400 text-justify mb-8">${overview.substring(0,200)}...</p>
+                                    <button id="btn-show-detail" class="absolute bottom-2 right-2 text-white bg-indigo-600 py-1.5 px-3 cursor-pointer hover:bg-indigo-700 active:bg-indigo-800 rounded-sm mt-2 md:py-2 md:px-4">Show Detail</button>`;
             moviesContainer.appendChild(movieCard);
+
+           
         })
     }
 
@@ -114,13 +123,57 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
     
+    function showDetail({title, poster_path, overview}) {
+        modalBoxContainer.innerHTML = ` <div id="modal-content" class="w-[80%] xl:max-w-3xl bg-neutral-800 mx-auto rounded-sm relative p-4 transform -translate-y-full opacity-0 transition-all duration-500 ease-out md:flex">
+                                            <button id="btn-close-modal-box" class="bg-red-600 hover:bg-red-700 active:bg-red-800 absolute -top-2 -right-2 text-white p-2 rounded-full cursor-pointer">
+                                                <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" fill="#ffffff"/></svg>
+                                            </button>
+                                            <img src="${API_IMAGE_URL}${poster_path}" alt="Gambar" class="mx-auto w-40 md:mr-4">
+                                            <div class="info-detail">
+                                                <h3 class="text-white text-center mt-2">${title}</h3>
+                                                <p class="text-gray-400">Release : </p>
+                                                <p class="text-gray-400">Overview : ${overview}</p>
+                                                <p class="text-gray-400">Popularity : </p>
+                                            </div>
+                                        </div>`
+        modalBoxContainer.classList.remove('hidden');
+        modalBoxContainer.classList.add('flex');
+        setTimeout(() => {
+            const modalContent = document.getElementById('modal-content');
+            modalContent.classList.remove('-translate-y-full', 'opacity-0');
+            modalContent.classList.add('translate-y-0', 'opacity-100');
+        }, 10);
+
+        const btnCloseModalBox = document.getElementById('btn-close-modal-box');
+        btnCloseModalBox.addEventListener('click', () => {
+            modalBoxContainer.classList.add('hidden');
+            modalBoxContainer.classList.remove('flex');
+            modalBoxContainer.innerHTML = '';
+        })
+    }
 
 
 
     updatePage();
 
+    moviesContainer.addEventListener('click', (e) => {
+        if(e.target && e.target.id === 'btn-show-detail') {
+            const card = e.target.closest('div');
+            const title = card.querySelector('h3').textContent;
+            const overview = card.querySelector('p').textContent;
+            const imgSrc = card.querySelector('img').getAttribute('src');
+            const poster_path = imgSrc.replace('https://image.tmdb.org/t/p/w1280', '');
+
+            showDetail({title, poster_path, overview});
+        }
+    })
+
     title.addEventListener('click', () => {
         location.reload();
     });
+
+
+    
+    
 });
 
